4ea99f2f22819d8589bc533a3b450e63baea7708
manak <manakpandey@gmail.com>
2019-07-03 16:53:14 +0530

    enabled shrink

--- app/src/main/java/com/mdev/revit/widget/TimetableWidget.kt
+++ app/src/main/java/com/mdev/revit/widget/TimetableWidget.kt
@@ -4,7 +4,6 @@ import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import android.widget.RemoteViews
 import com.mdev.revit.R
 
